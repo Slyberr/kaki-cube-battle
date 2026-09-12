7bf403821bde2ad23ed3a90ee80f7ac530540759
Slyberr <slybergame@gmail.com>
2026-09-13 00:12:58 +0200

    update header to AGPL

--- frontend/server/plugins/socketio.ts
+++ frontend/server/plugins/socketio.ts
@@ -1,10 +1,7 @@
 /*
  * Kaki Cube — Copyright (C) 2026 Louis Presti
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * See the LICENSE file at the root of this repository for full terms.
+ * Licensed under AGPL-3.0. See LICENSE file or
+ * https://www.gnu.org/licenses/agpl-3.0.html
  */
 
 import { CorsOptions } from 'cors';
