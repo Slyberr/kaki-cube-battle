f7b12b44a3e9287e0dd16b69536e76ff614732af
Slyberr <slybergame@gmail.com>
2026-09-13 17:19:48 +0200

    try with rensend HTTP

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
 
   runtimeConfig : {
     mailUser : '',
-    mailPassword: ''
+    resendApiKey: '',
   },
 
   nitro: {
--- frontend/server/api/feedback.post.ts
+++ frontend/server/api/feedback.post.ts
@@ -4,35 +4,24 @@
  * https://www.gnu.org/licenses/agpl-3.0.html
  */
 
-import nodemailer from "nodemailer";
+
+import { Resend } from "resend";
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const config = useRuntimeConfig(event);
 
   const { pseudo, mail, message, type } = body;
-  
-  try {
-    
-    const transported = nodemailer.createTransport({
-      host: "smtp.ionos.fr",
-      port: 587,
-      secure: false,
-      auth: {
-        user: config.mailUser,
-        pass: config.mailPassword,
-      },
-      connectionTimeout :10000,
-    });
+  const resend = new Resend(config.resendApiKey);
 
-    await transported.sendMail({
-      from: `"Kaki Cube Battle" <${config.mailUser}>`,
-      to: config.mailUser,
+  try {
+    resend.emails.send({
+      from: `KakiCube <${config.mailUser}>`,
+      to: [config.mailUser],
       subject: `Un retour ${type} a été envoyé par ${pseudo.length > 0 ? pseudo : "un anonyme"} !`,
-      text: `${message} \n ${mail.length > 0 ? "Adresse mail de réponse: " + mail : "sans mail renseigné"}`,
+      text:  `${message} \n ${mail.length > 0 ? "Adresse mail de réponse: " + mail : "sans mail renseigné"}`,
     });
   } catch (e: any) {
-   
     throw createError({
       statusCode: 500,
       message:
